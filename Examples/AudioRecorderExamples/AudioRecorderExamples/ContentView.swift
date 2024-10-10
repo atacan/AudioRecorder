149f80121e942a0fa27fed9bc1ca696518ed55d7
atacan <atacandur@icloud.com>
2024-10-10 23:34:50 +0200

    example with deepgram library

--- Examples/AudioRecorderExamples/AudioRecorderExamples/ContentView.swift
+++ Examples/AudioRecorderExamples/AudioRecorderExamples/ContentView.swift
@@ -7,37 +7,40 @@ import SwiftUI
 import AudioDataStreamClient
 import Dependencies
 import AVFoundation
+import Deepgram
+import DeepgramDependency
+import WebSocketDependency
 
-private let apiKey = "Token c58670fbc95fc9a2151a712e1755f701e4bfd2b0"
+private let apiKey = "Token "
 
 @Observable
 class ContentModel {
     var dataCount = 0
     var data = Data()
     let task: Task<(), any Error>? = nil
-    
-    
+
+
     func start() {
         @Dependency(\.audioDataStream) var audioDataStreamClient
         @Dependency(\.webSocket) var webSocketClient
         @Dependency(\.continuousClock) var clock
-        
-        Task {
-            for try await data in await audioDataStreamClient.startTask() {
-//                try await Task.sleep(nanoseconds: NSEC_PER_SEC)
-//                dump(data)
+
+//        Task {
+//            for try await data in await audioDataStreamClient.startTask() {
+////                try await Task.sleep(nanoseconds: NSEC_PER_SEC)
+////                dump(data)
 //                dataCount += data.count
-                self.data += data
-                try await webSocketClient.send(id: WebSocketClient.ID(), message: .data(data))
-            }
-        }
-        
+//                self.data += data
+//                try await webSocketClient.send(id: WebSocketClient.ID(), message: .data(data))
+//            }
+//        }
+
         Task {
 //            let url = URL(string: "wss://echo.websocket.events")!
             let url = URL(string: "wss://api.deepgram.com/v1/listen?encoding=linear16&sample_rate=48000&channels=1&model=nova&smart_format=true&filler_words=true")!
             var urlRequest = URLRequest(url: url)
             urlRequest.setValue(apiKey, forHTTPHeaderField: "Authorization")
-            
+
             let actions = await webSocketClient.open(
                 id: WebSocketClient.ID(),
                 url: urlRequest,
@@ -45,6 +48,7 @@ class ContentModel {
             )
             await withThrowingTaskGroup(of: Void.self) { group in
                 for await action in actions {
+                    print("action", action)
                     // NB: Can't call `await send` here outside of `group.addTask` due to task local
                     //     dependency mutation in `Effect.{task,run}`. Can maybe remove that explicit task
                     //     local mutation (and this `addTask`?) in a world with
@@ -54,31 +58,71 @@ class ContentModel {
                     case .didOpen:
                         group.addTask {
                             while !Task.isCancelled {
-                                try await clock.sleep(for: .seconds(10))
+                                try await clock.sleep(for: .seconds(1))
                                 try? await webSocketClient.sendPing(id: WebSocketClient.ID())
+                                try await webSocketClient.send(id: WebSocketClient.ID(), message: .string(##"{"type": "KeepAlive"}"##))
+                            }
+                        }
+                        group.addTask {
+                            for try await data in await audioDataStreamClient.startTask() {
+                //                try await Task.sleep(nanoseconds: NSEC_PER_SEC)
+                //                dump(data)
+//                                dataCount += data.count
+//                                self.data += data
+                                try await webSocketClient.send(id: WebSocketClient.ID(), message: .data(data))
                             }
                         }
                         group.addTask {
                             for await result in try await webSocketClient.receive(id: WebSocketClient.ID()) {
 //                                await send(.receivedSocketMessage(result))
-                                dump(result)
+                                switch result {
+                                case .success(let message):
+                                    switch message {
+                                    case .data(let data):
+                                        print("DATA")
+                                    case let .string(text):
+                                        let decoder = JSONDecoder()
+                                        do {
+                                            let deepgramResponse = try decoder.decode(DeepgramStreamingResponse.self, from: text.data(using: .utf8)!)
+                                            let firstAlternative = deepgramResponse.channel.alternatives[0]
+                                            print(firstAlternative.transcript)
+                                        } catch {
+                                            let deepgramResponse = try decoder.decode(DeepgramFinalizeConfirmation.self, from: text.data(using: .utf8)!)
+                                            print(deepgramResponse)
+                                        }
+                                    }
+                                case .failure(let failure):
+                                    dump(failure)
+                                }
                             }
                         }
-                    case .didClose:
+                    case let .didClose(code, data):
+                        dump(code.rawValue)
+//                        dump(data)
+//                        dump(String(data: data!, encoding: .utf8))
                         return
                     }
                 }
             }
         }
     }
-    
+
     func stop(){
         @Dependency(\.audioDataStream) var audioDataStreamClient
-        
+
+        @Dependency(\.webSocket) var webSocketClient
+
         Task {
             await audioDataStreamClient.stop()
+            
+            try await webSocketClient.send(id: WebSocketClient.ID(), message: .string("""
+                {
+                  "type": "Finalize"
+                }
+                """))
+//            try await webSocketClient.close(id: WebSocketClient.ID(), code: .normalClosure, reason: nil)
         }
-        
+
 //        do {
 //            let player = try AVAudioPlayer(data: self.data, fileTypeHint: AVFileType.wav.rawValue)
 //            player.prepareToPlay()
@@ -91,7 +135,7 @@ class ContentModel {
 
 struct ContentView: View {
     @State var model = ContentModel()
-    
+
     var body: some View {
         Button {
             model.start()
@@ -104,7 +148,7 @@ struct ContentView: View {
             }
             .padding()
         } // <-Button
-        
+
         Button {
             model.stop()
         } label: {
--- Package.swift
+++ Package.swift
@@ -11,7 +11,6 @@ let package = Package(
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(name: "AudioRecorder", targets: ["AudioRecorder"]),
         .library(name: "AudioRecorderClient", targets: ["AudioRecorderClient"]),
         .library(name: "AudioDataStreamClient", targets: ["AudioDataStreamClient"]),
     ],
@@ -21,11 +20,6 @@ let package = Package(
         .package(path: "../SystemSoundDependency"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "AudioRecorder"
-        ),
         .target(
             name: "AudioRecorderClient",
             dependencies: [
@@ -40,10 +34,6 @@ let package = Package(
                 .product(name: "CasePaths", package: "swift-case-paths"),
                 .product(name: "SystemSoundClient", package: "SystemSoundDependency"),
             ]
-        ),
-        .testTarget(
-            name: "AudioRecorderTests",
-            dependencies: ["AudioRecorder"]
         ),
         .testTarget(name: "AudioRecorderClientTests", dependencies: ["AudioRecorderClient"])
     ]
